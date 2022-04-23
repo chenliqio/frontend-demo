1c64fd6c207587acd8676afb95a426e476b30bb0
lance10030 <lance10030@163.com>
2022-04-23 22:22:40 +0800

    feat: js-task1

---
+++ js-task1/index.js
@@ -0,0 +1,29 @@
+window.onload =()=>{
+    // 1.获取元素
+    let doms=[];
+    // 2. 获取随机三个元素
+    function get3ElementFromDoms(){
+        return []
+    }
+    function get3RandomColor(){
+        return [];
+    }
+    function ChangeColor(el,colors){
+        el.forEach((item,index)=>{
+            item.style.backgroundColor=colors[index]
+        })
+    }
+    let btn_start = document.getElementById('start');
+    let btn_end = document.getElementById('end');
+    let interval;
+    btn_start.addEventListener('click',()=>{
+        interval = setInterval(()=>{
+            let threeEl = get3ElementFromDoms()
+            let ThreeColor = get3RandomColor();
+            ChangeColor(threeEl,ThreeColor)
+        }, 2000)
+    })
+    btn_end.addEventListener('click',()=>{
+        clearInterval(interval)
+    })
+}
